f4195acd6e89ce3eed89c24d2d65a845e14bd70a
Marcus Chiang <86793948+KingOfRedLions@users.noreply.github.com>
2021-10-29 15:53:17 -0400

    Update runActual.py

--- runActual.py
+++ runActual.py
@@ -130,7 +130,8 @@ def example_theory():
     E.add_constraint(H_1&JUMP_2)#Player 2 may jump to not get hit by a player 1 H
     E.add_constraint(H_2&JUMP_1)#The converse is also true
     E.add_constraint(((T_1&T_2)&(adjacent))>>bothNeutral)#Throw break
-    E.add_constraint((B_1)&(T_2))#Throwing works on a blocking opponent
+    E.add_constraint(((B_1)&(T_2))&adjacent) #Throwing a blocking opponent works
+
 
     return E
 
