ccfed5bd3b8ec4674e80bb53492abe776ce08aa0
sophellwood <19se@queensu.ca>
2021-12-06 11:21:00 -0500

    Update board.py

--- board.py
+++ board.py
@@ -1,7 +1,20 @@
 from tabulate import tabulate
 
-list1 = [['a','b','c'],['d','e','f'],['g','h','i']]
 
-print(tabulate(list1))
+#list1 = [['a','b','c'],['d','e','f'],['g','h','i']]
 
-propValueTable = [[],[]]#First row will be proposition names, second will be their truth values
+#print(tabulate(list1))
+
+#propValueTable = [[],[]]#First row will be proposition names, second will be their truth values
+
+# import module
+from tabulate import tabulate
+  
+# assign data
+mydata = [{"a","b","c","d   "}]
+  
+# create header
+head = ["Player 1 attack", "Player 2 attack", "Player 1 position", "Player 2 position"]
+  
+# display table
+print(tabulate(mydata, headers=head, tablefmt="grid"))
