fb9fc074dc3745d1ff9a10efe2b81672cddddd99
Marcus Chiang <86793948+KingOfRedLions@users.noreply.github.com>
2021-12-07 19:13:23 -0500

    Update run.py

--- run.py
+++ run.py
@@ -25,6 +25,8 @@ class p1Action:
         self.data = data
     def __repr__(self):
         return f"p1.{self.data}"
+
+
 P_1 = p1Attack("Punch",1) #Player 1 performs a punch (adjacent)
 K_1 = p1Attack("Kick",2) #Player 1 performs a kick (one space between)
 T_1 = p1Attack("Throw",0) #Player 1 performs a throw (adjacent)
@@ -48,7 +50,7 @@ class p2Action:
         self.data = data
 
     def __repr__(self):
-        return f"p2position.{self.data}"
+        return f"p2.{self.data}"
 
 #Propositions for player 2 
 P_2 = p2Attack("Punch",1) #Player 2 performs a punch
@@ -70,7 +72,6 @@ class p1Position:
         self.position = position
     def __repr__(self):
         return f"p1Position.{self.position}"
-
 @constraint.exactly_one(E)
 @proposition(E)
 class p2Position:
@@ -79,10 +80,9 @@ class p2Position:
     def __repr__(self):
         return f"p2Position.{self.position}"
 
-p1PositionArray = [p1Position(1),p1Position(2),p1Position(3),p1Position(4),p1Position(5),p1Position(6)]
-p2PositionArray = [p2Position(1),p2Position(2),p2Position(3),p2Position(4),p2Position(5),p2Position(6)]
-for i in range(len(p1PositionArray)-1):#Both players may not be in the same position
-    E.add_constraint(~(p1PositionArray[i]&p2PositionArray[i]))
+p1PositionArray = [p1Position(1),p1Position(2),p1Position(3),p1Position(4),p1Position(5),p1Position(6),p1Position(7),p1Position(8),p1Position(9),p1Position(10)]
+p2PositionArray = [p2Position(1),p2Position(2),p2Position(3),p2Position(4),p2Position(5),p2Position(6),p2Position(7),p2Position(8),p2Position(9),p2Position(10)]
+
 #Propositions pertaining to both players
 p1AttackArray = [P_1,K_1,T_1,H_1,SHORYU_1]
 p1ActionArray = [B_1,FJUMP_1,NJUMP_1]
@@ -91,15 +91,26 @@ p2ActionArray = [B_2,FJUMP_2,NJUMP_2]
 eitherJump1 = (FJUMP_1|NJUMP_1)
 eitherJump2 = (FJUMP_2|NJUMP_2)
 
+@proposition(E)
+class counter:
+    def __init__(self,data):
+        self.data = data
+    def __repr__(self):
+        return f"counter.{self.data}"
 def rangeConstraint(action,p1position,p2position):
     if (abs(p1position.position-p2position.position)>action.range):
         E.add_constraint(~(p1position&p2position&action))
-    
-#############################################
+def counterHit(p1action, p2action):
+    if (p1action.range>p2action.range):
+        C = counter("Counter")
+        
 
+
+#############################################
 def flawlessDefence():
+    for p1position,p2position in zip(p1PositionArray,p2PositionArray):#Both players may not be in the same position
+        E.add_constraint(~(p1position&p2position))
     E.add_constraint(~B_2)
-    #Player 1 limitations
     for subset in combination(p1ActionArray,2):#Cannot perform any 2 actions at once
         E.add_constraint(~(subset[0]&subset[1]))
     for subset in combination(p1AttackArray,2):#Cannot perform any 2 attacks at once
@@ -108,6 +119,9 @@ def flawlessDefence():
         E.add_constraint(~(p1AttackArray[i]&B_1))
     for i in range(1,len(p1ActionArray)-1):#Cannot perform either jump and block at the same time
         E.add_constraint(~(B_1&p1AttackArray[i]))
+    for i in range(2,len(p1AttackArray)-1):#Cannot perform attacks that are not punch or kick and jump at the same time
+        E.add_constraint(~(NJUMP_1&p1AttackArray[i]))
+        E.add_constraint(~(FJUMP_1&p1AttackArray[i]))
     #Player 2 limitations
     for subset in combination(p2ActionArray,2):#Cannot perform any 2 actions at once
         E.add_constraint(~(subset[0]&subset[1]))
@@ -117,6 +131,9 @@ def flawlessDefence():
         E.add_constraint(~(p2AttackArray[i]&B_1))
     for i in range(1,len(p2ActionArray)-1):#Cannot perform either jump and block at the same time
         E.add_constraint(~(B_1&p2AttackArray[i]))
+    for i in range(2,len(p2AttackArray)-1):
+        E.add_constraint(~(NJUMP_2&p2AttackArray[i]))
+        E.add_constraint(~(FJUMP_2&p2AttackArray[i]))
     #P1 Range
     for p1position in p1PositionArray:
         for p2position in p2PositionArray:
@@ -127,11 +144,7 @@ def flawlessDefence():
         for p1position in p1PositionArray:
             for attack in p2AttackArray:
                 rangeConstraint(attack,p2position,p1position)
-
-    #From here on, range is covered. Do not need to include range in constraints since they already may not happen when not in range.
     
-    
-    #Compiling and returning theory
     return E
 
 if __name__ == "__main__":
@@ -149,7 +162,7 @@ if __name__ == "__main__":
     print("\nLikelihood for player 1 to perform a certain action:")
     p1ActionArray = [P_1,K_1,H_1,SHORYU_1,T_1,NJUMP_1,FJUMP_1,B_1]
     #p2ActionArray = [P_2,K_2,H_1,SHORYU_1,T_1,NJUMP_2,FJUMP_2,B_2]
-    for v,vn in zip(p1ActionArray, 'PKHSTNFB'):
+    for v,vn in zip(p1ActionArray, 'PKHSTNFBC'):
         print(" %s: %.2f" % (vn, likelihood(D, v)))
     print(len(D.vars()))
     print(len(D))
